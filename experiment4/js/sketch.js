a53e2cc99a31dad2a88868a6f44d18f176b2bc1d
Beepered <bktrieu@gmail.com>
2024-02-01 20:45:23 -0800

    experiment 4 test push

--- experiment4/js/sketch.js
+++ experiment4/js/sketch.js
@@ -1,67 +1,48 @@
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+let img
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
-
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
-
-// Globals
-let myInstance;
-let canvasContainer;
-
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
-}
-
-// setup() function is called once when the program starts
 function setup() {
-    // place our canvas, making it fit our container
-    canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-    canvas.parent("canvas-container");
-    // resize canvas is the page is resized
-    $(window).resize(function() {
-        console.log("Resizing...");
-        resizeCanvas(canvasContainer.width(), canvasContainer.height());
-    });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
+  createCanvas(650, 500);
 
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
+  // Create an Audio input
+  mic = new p5.AudioIn();
+
+  // start the Audio Input.
+  // By default, it does not .connect() (to the computer speakers)
+  mic.start();
+  
+  img = loadImage("image.png")
 }
 
-// draw() function is called repeatedly, it's the main animation loop
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
+  background(220);
 
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
-}
+  // Get the overall volume (between 0 and 1.0)
+  let vol = mic.getLevel();
 
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
+  // Draw an ellipse with height based on volume
+  let h = height - (vol * height * 1.3)
+  
+  let img_width = img.width + (vol * 30)
+  let img_height = img.height + (vol * 100)
+  
+  tint(255, 90); // opacity
+  image(img, (width / 2) - img_width / 2 - 270, h - img_height, img_width, img_height);
+  image(img, (width / 2) - img_width / 2 + 270, h - img_height, img_width, img_height);
+  
+  img_width = img.width + (vol * 80)
+  img_height = img.height + (vol * 180)
+  tint(255, 130); // opacity
+  image(img, (width / 2) - img_width / 2 - 180, h - img_height, img_width, img_height);
+  image(img, (width / 2) - img_width / 2 + 180, h - img_height, img_width, img_height);
+  
+  img_width = img.width + (vol * 120)
+  img_height = img.height + (vol * 350)
+  tint(255, 190);
+  image(img, (width / 2) - img_width / 2 - 90, h - img_height, img_width, img_height);
+  image(img, (width / 2) - img_width / 2 + 90, h - img_height, img_width, img_height);
+  
+  img_width = img.width + (vol * 300)
+  img_height = img.height + (vol * 750)
+  tint(255, 255);
+  image(img, (width / 2) - img_width / 2, h - img_height, img_width, img_height);
 }
